b21c59d5b48533a93188c94ff440b0e56fcb781e
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-03-20 19:44:38 +0200

    Add tests

--- handler_test.go
+++ handler_test.go
@@ -8,3 +8,33 @@ import (
   "github.com/stretchr/testify/assert"
   "github.com/stretchr/testify/require"
 )
+
+func TestComputeHandler_Success(t *testing.T) {
+	input := "+ 2 3"
+	reader := strings.NewReader(input)
+	var buf bytes.Buffer
+
+	handler := &ComputeHandler{
+		Input:  reader,
+		Output: &buf,
+	}
+
+	err := handler.Compute()
+	require.NoError(t, err, "compute must successfully process a valid input expression")
+
+	expected := "(+ 2 3)"
+	assert.Equal(t, expected, buf.String(), "The result obtained should correspond to the expected one.")
+}
+
+func TestComputeHandler_InvalidInput(t *testing.T) {
+	reader := strings.NewReader("")
+	var buf bytes.Buffer
+
+	handler := &ComputeHandler{
+		Input:  reader,
+		Output: &buf,
+	}
+
+	err := handler.Compute()
+	require.Error(t, err, "Compute should return an error on an empty input expression")
+}
