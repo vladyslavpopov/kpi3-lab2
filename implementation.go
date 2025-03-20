56e01352a75e7ea88418e9a16813aa5d92d6f2df
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-03-20 19:10:05 +0200

    Add converting function realization

--- implementation.go
+++ implementation.go
@@ -1,9 +1,66 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func PrefixToLisp(input string) (string, error) {
+	if len(input) == 0 {
+		return "", fmt.Errorf("empty input")
+	}
+	tokens := strings.Fields(input)
+	index := 0
+
+	result, err := parsePrefix(tokens, &index, true)
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
+func parsePrefix(tokens []string, index *int, topLevel bool) (string, error) {
+	if *index >= len(tokens) {
+		return "", fmt.Errorf("invalid expression")
+	}
+
+	token := tokens[*index]
+	*index++
+
+	if isOperator(token) {
+		operands := []string{}
+
+		for i := 0; i < 2; i++ {
+			operand, err := parsePrefix(tokens, index, false)
+			if err != nil {
+				return "", err
+			}
+			operands = append(operands, operand)
+		}
+
+		if topLevel {
+			for *index < len(tokens) {
+				operand, err := parsePrefix(tokens, index, false)
+				if err != nil {
+					return "", err
+				}
+				operands = append(operands, operand)
+			}
+		}
+
+		return fmt.Sprintf("(%s %s)", tokenToLisp(token), strings.Join(operands, " ")), nil
+	}
+
+	return token, nil
+}
+
+func isOperator(token string) bool {
+	return token == "+" || token == "-" || token == "*" || token == "/" || token == "^"
+}
+
+func tokenToLisp(token string) string {
+	if token == "^" {
+		return "pow"
+	}
+	return token
 }
